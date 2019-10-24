2f1c47e1454af57b3f5ea6883c126861ddee4e95
Cello Ward <celloward@gmail.com>
2019-10-24 09:12:33 -0500

    Create guess limiter

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,41 +1,65 @@
 require "sinatra"
 require "sinatra/reloader"
 
-SECRET_NUM = rand(101)
-result = nil
-color = "white"
+class WebGuesser
+  attr_reader :secret_num
 
-def check_guess guess
-  if guess.to_i > SECRET_NUM
-    result = "Too high!"
-  elsif guess.to_i < SECRET_NUM
-    result = "Too low!"
-  elsif guess.to_i == SECRET_NUM
-    result = "Correct!"
-  else
-    result = "Not a valid guess. Please enter an integer between 0 and 100"
+  def initialize
+    @secret_num = rand(101)
+    result = nil
+    color = "white"
   end
 
-  if (guess.to_i - SECRET_NUM).abs > 5
-    result = "Way " + result.downcase
-  else
-    result
-  end 
+  def check_guess guess
+    @@guesses_left -= 1
+    if guess.to_i > @secret_num
+      result = "Too high!"
+    elsif guess.to_i < @secret_num
+      result = "Too low!"
+    elsif guess.to_i == @secret_num
+      result = "Correct!"
+    else
+      result = "Not a valid guess. Please enter an integer between 0 and 100"
+    end
+
+    if (guess.to_i - @secret_num).abs > 5
+      result = "Way " + result.downcase
+    else
+      result
+    end 
+  end
+
+  def colorize result
+    if result.match(/Correct/)
+      color = "green"
+    elsif result.match(/Way/)
+      color = "red"
+    else
+      color = "yellow"
+    end
+  end
+
+  def game_status result
+    if @@guesses_left == 0 || result.match(/Correct/)
+      @secret_num = rand(101)
+      @@guesses_left = 6
+      color = "white"
+      result = nil
+    end
+  end
 end
 
-def colorize result
-  if result.match(/Correct/)
-    color = "green"
-  elsif result.match(/Way/)
-    color = "red"
-  else
-    color = "yellow"
-  end
+@@guesses_left = 6
+game = WebGuesser.new
+
+before do
+  @secret_num = game.secret_num
 end
 
 get "/" do
   guess = params["guess"]
-  result = check_guess(guess)
-  color = colorize(result)
-  erb :index, :locals => {:secret_num => SECRET_NUM, :result => result, :color => color}
+  result = game.check_guess(guess)
+  color = game.colorize(result)
+  game.game_status(result)
+  erb :index, :locals => {:secret_num => @secret_num, :result => result, :color => color, :guesses => @@guesses_left}
 end
